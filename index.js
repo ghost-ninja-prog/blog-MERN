933b733b7c105ebc1fe6d722cc9faa8c5d7b38cf
ghost-ninja-prog <alex19892140@inbox.ru>
2023-03-10 13:47:15 +0700

    end backend

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 
-import { checkAuth } from './utils/index.js';
+import { checkAuth, handleValidationErrors } from './utils/index.js';
 
-import * as UserController from './controllers/UserController.js';
-import { loginValidation, registerValidation } from './validations.js';
+import { UserController, PostController } from './controllers/index.js';
+import { loginValidation, postCreateValidation, registerValidation } from './validations.js';
 
 mongoose
   .connect('mongodb+srv://alex19892140:0FcdmOeHM2ql9BKo@cluster0.gko4myi.mongodb.net/blog?retryWrites=true&w=majority')
@@ -12,12 +13,39 @@ mongoose
   .catch((err) => console.log('DB error', err));
 
 const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage })
+
 app.use(express.json());
+app.use('/uploads', express.static('uploads'))
 
 
-app.post('/auth/login', loginValidation, UserController.login);
-app.post('/auth/register', registerValidation, UserController.register);
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`
+  })
+})
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth,postCreateValidation, handleValidationErrors, PostController.update);
+
+
 
 
 app.listen(process.env.PORT || 4444, (err) => {

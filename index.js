38e9cfe593d8d8031e962daf4b8e0dd8a40c9cf1
ghost-ninja-prog <alex19892140@inbox.ru>
2023-03-12 00:22:50 +0700

    change index.js

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
   })
 })
 
+app.get('/posts/tags', PostController.getLastTags)
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
